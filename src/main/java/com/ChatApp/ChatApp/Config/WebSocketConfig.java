dc425e0bd39a920189f76344cec0f39b9bdabedc
Akash Agrawal <akashji93685@gmail.com>
2025-07-28 12:42:04 +0530

    Added src folder after removing submodule issue

---
+++ src/main/java/com/ChatApp/ChatApp/Config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.ChatApp.ChatApp.Config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker // it tells spring that it will handle real time message with the help of web socket
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+registry.enableSimpleBroker("/topic");
+registry.setApplicationDestinationPrefixes("/app");
+
+    }
+}
